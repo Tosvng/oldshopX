0285d51585fa8240ceabc329eace78f5187e4203
Tosvng <altosintiams@gmail.com>
2022-11-29 14:00:46 -0600

    deleted duplicate folder

--- shopX/settings.py
+++ shopX/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os
-import django_heroku
-import dj_database_url
+# import os
+# import django_heroku
+# import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static_root'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
@@ -131,4 +131,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # activate django-heroku
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
