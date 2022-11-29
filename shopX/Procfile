992767b2362cedc9f5794d4abc2627b545322765
Tosvng <altosintiams@gmail.com>
2022-11-29 14:08:41 -0600

    edited settings.py

--- shopX/Procfile
+++ shopX/Procfile
@@ -1,1 +1,1 @@
-web: gunicorn shopX.wsgi:application --log-file -
+web: gunicorn shopX.wsgi
--- shopX/settings.py
+++ shopX/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-# import os
-# import django_heroku
-# import dj_database_url
+import os
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-8)vc7)@8o(n*-#4i!^)t@60o=$mk4(v0u8wb79$^+jp)m5vzs6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shopx-97b.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static_root'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
@@ -131,4 +131,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # activate django-heroku
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
